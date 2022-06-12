08f41eba1daaf6472a2946820f0270817de61e1d
javeriatabassum145 <javeria.tabassum145@gmail.com>
2022-06-12 12:31:41 +0500

    Verion2

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,8 +8,8 @@ export class NewsItem extends Component {
         <div className="card m-3">
           <img src={imageUrl} className="card-img-top" alt="..." style={{ height: "12rem" }}/>
           <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
+            <h5 className="card-title">{title}....</h5>
+            <p className="card-text">{description}....</p>
             <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
               Read More
             </a>
